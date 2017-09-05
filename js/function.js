a7ac1853737305f513fd25f71c8f40d7e6a6511f
ttoha <toooxa@gmail.com>
2017-09-06 01:36:46 +0300

    allCotage

--- js/function.js
+++ js/function.js
@@ -95,6 +95,37 @@ $( document ).ready(function() {
         prevArrow: '<div data-role="none" class="slick-prev slick-arrow" aria-label="Previous" role="button"><span><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 477.175 477.175" style="enable-background:new 0 0 477.175 477.175;" xml:space="preserve"><g><path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"/></g></svg></span></div>',
         nextArrow: '<div data-role="none" class="slick-next slick-arrow" aria-label="Next" role="button"><span><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 477.175 477.175" style="enable-background:new 0 0 477.175 477.175;" xml:space="preserve"><g><path d="M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z"/></g></svg></span></div>'
     });
+    $('.cottage-slider').slick({
+        dots: false,
+        speed: 300,
+          centerMode: true,
+          infinite: true,
+          centerPadding: '295px',
+        slidesToShow: 1,
+        draggable:false,
+        adaptiveHeight: true,
+        autoplay: false,
+        prevArrow: '<div data-role="none" class="slick-prev slick-arrow" aria-label="Previous" role="button"><span><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 477.175 477.175" style="enable-background:new 0 0 477.175 477.175;" xml:space="preserve"><g><path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"/></g></svg></span></div>',
+        nextArrow: '<div data-role="none" class="slick-next slick-arrow" aria-label="Next" role="button"><span><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 477.175 477.175" style="enable-background:new 0 0 477.175 477.175;" xml:space="preserve"><g><path d="M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z"/></g></svg></span></div>',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                centerPadding: '0'
+              }
+            },{
+              breakpoint: 768,
+              settings: {
+                centerPadding: '0'
+              }
+            },{
+              breakpoint: 480,
+              settings: {
+                centerPadding: '0'
+              }
+            }
+        ]
+    });
 
     $('.small-slider').slick({
         dots: false,
@@ -117,15 +148,6 @@ $( document ).ready(function() {
         ]
     });
 
-    $('.all-cottage-item-hover').on('mouseover', function(){
-        var par = $(this).parent('.all-cottage-item').outerHeight();
-        $(this).parent('.all-cottage-item').css('height', par);
-        $(this).css('position','absolute');
-        $(this).find('.extra').show();
-    });
-    $('.all-cottage-item-hover').on('mouseout', function(){
-        $(this).css('position','static');
-    });
 });
 
 
